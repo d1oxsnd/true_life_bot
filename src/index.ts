efb3a9e1887accb0fa91ace2449d46c97ce7245e
dioxsound <d1oxsnd>
2026-07-20 17:20:33 +0300

    feat(entrypoint): implement user commands handler and connect all architectural layers and boot grammy bot

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { Bot } from 'grammy'
+import { UserRepository } from './repositories/user.js'
+import { GameService } from './services/game.service.js'
+import { GuardMiddleware } from './handlers/guards.js'
+import { UserHandler } from './handlers/user.handler.js'
+
+const token = process.env.BOT_TOKEN
+if (!token) {
+	throw new Error('Ошибка: В .env не задан BOT_TOKEN!')
+}
+
+const userRepository = new UserRepository()
+const gameService = new GameService(userRepository)
+
+const guard = new GuardMiddleware(gameService)
+const userHandler = new UserHandler(gameService)
+
+const bot = new Bot(token)
+
+bot.command('start', ctx => userHandler.onStart(ctx))
+
+console.log('Бот True Life успешно запущен в режиме Long Polling...')
+bot.start()
