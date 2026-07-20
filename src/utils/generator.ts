752267fd9f4c31062739ccaa79de63c900ce75f1
dioxsound <d1oxsnd>
2026-07-20 14:43:28 +0300

    feat(utils): add custom ID generator

---
+++ src/utils/generator.ts
@@ -0,0 +1,15 @@
+import crypto from 'crypto';
+
+export function generateGameId(): string {
+  const alphabet = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789';
+  let id = '';
+  const bytes = crypto.randomBytes(6);
+  
+  for (let i = 0; i < 6; i++) {
+    if (i === 3) {
+      id += '-';
+    }
+    id += alphabet[bytes[i]! % alphabet.length];
+  }
+  return id;
+}
